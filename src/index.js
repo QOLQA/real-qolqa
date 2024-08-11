b3fc0298e2767303c53dee6a546fea850b8aa194
Anghelo Alagon <200518@unsaac.edu.pe>
2024-08-11 18:50:55 -0500

    add navbar.js in index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import setupToolbar from "./toolbar";
 import setupOutlineNavigation from "./features/outline_navigator";
 import setupZoomSlider from "./features/zoom_slider";
 import setupKeyboard from "./features/keyboard";
+import { haddleNavbar } from "./features/navbar";
 
 setupToolbar();
 setupOutlineNavigation();
 setupZoomSlider();
 setupKeyboard();
+haddleNavbar();
 
