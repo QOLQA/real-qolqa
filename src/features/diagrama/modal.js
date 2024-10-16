90bb68f80500df156f350a5a80c02bc8167db08c
roswell <106674098+welpmoz@users.noreply.github.com>
2024-10-16 10:51:51 -0500

    form center in middle and on top of all (#57)
    ### Changes
    
    The changes made are explained :
    
    - Added new styles for show add new attribute in the entire screen
    (`src/features/diagrama/modal.js`)
    
    ![image](https://github.com/user-attachments/assets/d3dba652-e008-4c3a-b69b-a3acc705acaf)
    
    - Update the logic when `Guardar` button is clicked
    (`src/features/diagrama/action.js`)
    
    ![image](https://github.com/user-attachments/assets/5f608ba9-e086-4364-bc6e-c76f802bd5c3)
    
    
    ---
    
    Close #41

--- src/features/diagrama/modal.js
+++ src/features/diagrama/modal.js
@@ -3,6 +3,7 @@ export default function showModalWindow(graph, title, content, width, height)
 		{
 			// // var background = document.createElement('div');
 			var background = document.getElementById('background-add-property');
+			background.classList.replace('hidden', 'flex');
 			background.appendChild(content);
 			console.log('showing modal window for add property');
 		};
